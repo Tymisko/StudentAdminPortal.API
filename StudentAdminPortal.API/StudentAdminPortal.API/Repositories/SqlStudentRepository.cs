38eb9ea9dff23ab5c52cb4f077efdaa0ff492012
Jan Urbaś <janurbas39@gmail.com>
2022-08-05 05:58:07 +0200

    Rename SqlServerRepository to SqlStudentRepository

--- StudentAdminPortal.API/StudentAdminPortal.API/Repositories/SqlStudentRepository.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Repositories/SqlStudentRepository.cs
@@ -1,22 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using StudentAdminPortal.API.DataModels;
 
 namespace StudentAdminPortal.API.Repositories
 {
-    public class SqlServerRepository : IStudentRepository
+    public class SqlStudentRepository : IStudentRepository
     {
         private readonly StudentAdminContext _context;
 
-        public SqlServerRepository(StudentAdminContext context)
+        public SqlStudentRepository(StudentAdminContext context)
         {
             _context = context;
         }
 
         public List<Student> GetStudents()
         {
-            return _context.Student.ToList();
+            return _context.Student.Include(nameof(Gender)).Include(nameof(Address)).ToList();
         }
     }
 }
