618089c8d309a507a5f26d1d2e960cee06f2ae33
Jan Urbaś <janurbas39@gmail.com>
2022-11-05 23:00:33 +0100

    Added database migrate in Main method of Program.cs

--- StudentAdminPortal.API/Program.cs
+++ StudentAdminPortal.API/Program.cs
@@ -6,6 +6,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using StudentAdminPortal.API.DataModels;
 
 namespace StudentAdminPortal.API
 {
@@ -13,7 +16,15 @@ namespace StudentAdminPortal.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<StudentAdminContext>();
+                db.Database.Migrate();
+            }
+            
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
