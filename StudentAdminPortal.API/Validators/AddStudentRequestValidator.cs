a1c4778cf6666a8e6929d445cd70413900bd995d
Jan Urbaś <janurbas39@gmail.com>
2022-11-06 17:13:17 +0100

    Implement AddStudentRequestValidator.cs

---
+++ StudentAdminPortal.API/Validators/AddStudentRequestValidator.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using FluentValidation;
+using StudentAdminPortal.API.DomainModels;
+using StudentAdminPortal.API.Repositories;
+
+namespace StudentAdminPortal.API.Validators
+{
+	public class AddStudentRequestValidator : AbstractValidator<AddStudentRequest>
+	{
+		public AddStudentRequestValidator(IStudentRepository studentRepository)
+		{
+			RuleFor(x => x.FirstName).NotEmpty();
+			RuleFor(x => x.LastName).NotEmpty();
+			RuleFor(x => x.DateOfBirth).NotEmpty();
+			RuleFor(x => x.Email).NotEmpty().EmailAddress();
+			RuleFor(x => x.Mobile).GreaterThan(12345678).LessThan(1234567890);
+			RuleFor(x => x.GenderId).NotEmpty().Must(id =>
+			{
+				var gender = studentRepository.GetGendersAsync().Result.ToList().FirstOrDefault(g => g.Id == id);
+				return gender != null;
+			}).WithMessage("Please select a valid gender");
+			RuleFor(x => x.PhysicalAddress).NotEmpty();
+			RuleFor(x => x.PostalAddress).NotEmpty();
+		}
+	}
+}
